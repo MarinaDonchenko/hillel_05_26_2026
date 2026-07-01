23c6e32860a5c611266ebf8187ed836fd6e9c918
Marina Donchenko <mari@MacBook-Pro-Marina.local>
2026-07-01 15:28:48 +0300

    Fix for homework_09

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -1,4 +1,8 @@
 class Rhombus:
+    def __init__(self, side_a, angle_a):
+        self.side_a = side_a
+        self.angle_a = angle_a
+
     def __setattr__(self, name, value):
         if name == 'side_a':
             if value <= 0:
@@ -13,15 +17,11 @@ class Rhombus:
             raise AttributeError("angle_b is calculated automatically by angle_a")
         else:
             object.__setattr__(self, name, value)
+
     def __str__(self):
         return (f"Rhombus: side_a={self.side_a}, "
                 f"angle_a={self.angle_a}, "
                 f"angle_b={self.angle_b}")
-rhombus = Rhombus()
-rhombus.side_a = 5
-rhombus.angle_a = 80
 
+rhombus = Rhombus(side_a=5, angle_a=80)
 print(rhombus)
-print(f"side_a: {rhombus.side_a}")
-print(f"angle_a: {rhombus.angle_a}")
-print(f"angle_b: {rhombus.angle_b}")
